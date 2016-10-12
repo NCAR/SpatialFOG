09aa86ac130ab245af361eb1f8ea658b96037077
Chris Burghart <burghart@ucar.edu>
2016-10-12 13:24:19 -0600

    Add accessor methods to RawSensorsPacket

--- RawSensorsPacket.h
+++ RawSensorsPacket.h
@@ -18,6 +18,55 @@ public:
   RawSensorsPacket(const void* raw, uint length);
 
   virtual ~RawSensorsPacket();
+
+  /// @brief Return the acceleration along the sensor X axis, m s-2
+  /// @return the acceleration along the sensor X axis, m s-2
+  float accelerometerX() const { return(_data._accelerometerX); }
+
+  /// @brief Return the acceleration along the sensor Y axis, m s-2
+  /// @return the acceleration along the sensor Y axis, m s-2
+  float accelerometerY() const { return(_data._accelerometerY); }
+
+  /// @brief Return the acceleration along the sensor Z axis, m s-2
+  /// @return the acceleration along the sensor Z axis, m s-2
+  float accelerometerZ() const { return(_data._accelerometerZ); }
+
+  /// @brief Return the rotation rate around the sensor X axis, rad s-1
+  /// @return the rotation rate around the sensor X axis, rad s-1
+  float gyroscopeX() const { return(_data._gyroscopeX); }
+
+  /// @brief Return the rotation rate around the sensor Y axis, rad s-1
+  /// @return the rotation rate around the sensor Y axis, rad s-1
+  float gyroscopeY() const { return(_data._gyroscopeY); }
+
+  /// @brief Return the rotation rate around the sensor Z axis, rad s-1
+  /// @return the rotation rate around the sensor Z axis, rad s-1
+  float gyroscopeZ() const { return(_data._gyroscopeZ); }
+
+  /// @brief Return the magnetic field along the sensor X axis, mG
+  /// @return the magnetic field along th sensor X axis, mG
+  float magnetometerX() const { return(_data._magnetometerX); }
+
+  /// @brief Return the magnetic field along the sensor Y axis, mG
+  /// @return the magnetic field along th sensor Y axis, mG
+  float magnetometerY() const { return(_data._magnetometerY); }
+
+  /// @brief Return the magnetic field along the sensor Z axis, mG
+  /// @return the magnetic field along th sensor Z axis, mG
+  float magnetometerZ() const { return(_data._magnetometerZ); }
+
+  /// @brief Return the IMU temperature, deg C
+  /// @return the IMU temperature, deg C
+  float imuTemperature() const { return(_data._imuTemp); }
+
+  /// @brief Return the atmospheric pressure, Pa
+  /// @return the atmospheric pressure, Pa
+  float pressure() const { return (_data._pressure); }
+
+  /// @brief Return the pressure temperature (what is this?), deg C
+  /// @return the pressure temperature (what is this?), deg C
+  float pressureTemperature() const { return(_data._pressureTemp); }
+
 protected:
   /// @brief ANPP packet id for this packet type
   static const uint8_t _PACKET_ID = 28;
@@ -40,7 +89,7 @@ private:
     float _gyroscopeZ;      // rad/s
     float _magnetometerX;   // mGa
     float _magnetometerY;   // mGa
-    float _magnetomegerZ;   // mGa
+    float _magnetometerZ;   // mGa
     float _imuTemp;         // deg C
     float _pressure;        // Pa
     float _pressureTemp;    // deg C
